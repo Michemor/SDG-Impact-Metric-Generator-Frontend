8b31e11a585fe77377ddb94e303dafc835918933
Michemor <yatoijema@gmail.com>
2026-01-18 16:31:39 +0300

    pulled from main

---
+++ sdg-impact-dashboard/src/components/SideMenu.jsx
@@ -0,0 +1,9 @@
+import { Drawer } from "@mui/material";
+
+export default function SideMenu() {
+    return (
+        <Drawer variant="permanent" anchor="left">
+            <div>Side Menu Content</div>
+        </Drawer>
+    );
+}
