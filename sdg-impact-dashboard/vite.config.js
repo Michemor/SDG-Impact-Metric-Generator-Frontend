8851dd26acffbc25201c192ef6f291dbfcd687b8
Michemor <yatoijema@gmail.com>
2026-01-19 21:51:51 +0300

    Accepted changes to config file

--- sdg-impact-dashboard/vite.config.js
+++ sdg-impact-dashboard/vite.config.js
@@ -4,15 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-<<<<<<< HEAD
     react(),
-=======
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
->>>>>>> main
   ],
   server: {
     proxy: {
